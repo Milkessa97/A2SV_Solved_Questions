420ba3ec1dd2c3b840fda8667172b0912ab13f69
Milkessa Habtamu <milkessahabtamukebu@gmail.com>
2026-02-04 15:04:44 +0300

    Add swap_case function to change case of characters

---
+++ sWAP_cASE.py
@@ -0,0 +1,13 @@
+def swap_case(s):
+    res = ""
+    for char in s:
+        if char.upper() == char:
+            res += char.lower()
+        else:
+            res += char.upper()
+    return res
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
