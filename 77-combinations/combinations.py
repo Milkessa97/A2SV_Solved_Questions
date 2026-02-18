84f1383fc472574034d8c10ac704a6e38f52eca1
Milkessa Habtamu <milkessahabtamukebu@gmail.com>
2026-02-18 22:12:02 +0300

    Time: 117 ms (49.06%) | Memory: 61.4 MB (37.80%) - LeetSync

---
+++ 77-combinations/combinations.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        ans = []
+        def backtrack(i=1,arr=[]):
+            if len(arr) == k:
+                ans.append(arr.copy())
+                return
+            if i>n:
+                return
+
+            arr.append(i)
+            backtrack(i+1,arr)
+            arr.pop()
+            backtrack(i+1,arr)
+        backtrack()
+        return ans
+
