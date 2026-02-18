6614ccfb8791f3ead7f326d03a63bf52201c680c
Milkessa Habtamu <milkessahabtamukebu@gmail.com>
2026-02-18 20:04:25 +0300

    Time: 488 ms (6.45%) | Memory: 27.1 MB (7.02%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,22 +1,11 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        if len(names) == 1:
-            return names
-        
-        left, right = 0, 1
-
-        while left < len(names):
-            max_idx = left
-
-            while right < len(names):
-                if heights[right] > heights[max_idx]:
-                    max_idx = right
-                right += 1
-
-            names[left], names[max_idx] = names[max_idx], names[left]
-            heights[left], heights[max_idx] = heights[max_idx], heights[left]
-
-            left += 1
-            right = left + 1
-
-        return names
+        max_height = max(heights)+1
+        counter=[[] for i in range(max_height)]
+        for i in range(len(names)):
+            counter[heights[i]].append(names[i])
+        counter.reverse()
+        res=[]
+        for col in counter:
+            res.extend(col)
+        return res
