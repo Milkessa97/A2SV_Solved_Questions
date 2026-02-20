cfa9bbee703ec810b2ed48300dcc1c7fe90e07eb
Milkessa Habtamu <milkessahabtamukebu@gmail.com>
2026-02-20 17:39:56 +0300

    Time: 60 ms (5.12%) | Memory: 20.6 MB (28.54%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.py
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.py
@@ -3,14 +3,18 @@ class Solution:
         nums.sort()
         first = len(nums)-1
         perimeter = 0
+
         while first > 1:
             second = first-1
             third = second-1
+
             while third>=0:
                 if nums[second]+nums[third] > nums[first]:
                     return nums[second]+nums[third]+nums[first]
                 else:
                     second-=1
                     third-=1
+
             first-=1
+            
         return 0
