1457710ac2e8034aee07ac930a66713ff519a37f
Milkessa Habtamu <milkessahabtamukebu@gmail.com>
2026-02-24 19:14:55 +0300

    Time: 127 ms (68.05%) | Memory: 21.5 MB (10.60%) - LeetSync

--- 524-longest-word-in-dictionary-through-deleting/longest-word-in-dictionary-through-deleting.py
+++ 524-longest-word-in-dictionary-through-deleting/longest-word-in-dictionary-through-deleting.py
@@ -2,18 +2,23 @@ class Solution:
     def findLongestWord(self, s: str, dictionary: List[str]) -> str:
         check=Counter(s)
         res=''
+
         for word in dictionary:
             word_c = Counter(word)
+
             if word_c <= check:
                 p = 0
+                
                 for char in s:
                     if char == word[p]:
                         p+=1
+
                     if p == len(word):
                         if len(word) == len(res):
                             res = min(res,word)
                         elif len(word) > len(res):
                             res = word
                         break
+                        
         return res
                 
