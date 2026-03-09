5e8e8685fde404563fccfcdc30d95b677ebf6baa
Milkessa Habtamu <milkessahabtamukebu@gmail.com>
2026-03-09 19:25:13 +0300

    Time: 3 ms (40.89%) | Memory: 19.2 MB (88.69%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -1,7 +1,6 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
         path = [dir for dir in path.split('/')]
-        print(path)
         stack = []
         for dir in path:
             if dir == "." or dir == "":
