40629b2dd17bc374962a8f7ae1ab4a860b2c567a
Milkessa Habtamu <milkessahabtamukebu@gmail.com>
2026-03-09 19:31:44 +0300

    Time: 3 ms (40.89%) | Memory: 19.2 MB (98.06%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -2,16 +2,12 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
         path = path.split('/')
         stack = deque()
-
         for dir in path:
             if dir == "." or dir == "":
                 continue
-
             elif dir == "..":
                 if stack:
                     stack.pop()
-
             else:
                 stack.append(dir)
-                
         return '/'+'/'.join(stack)
