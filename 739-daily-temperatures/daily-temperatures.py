18528603936bedf1dfc888a1c5a44e9347001630
Milkessa Habtamu <milkessahabtamukebu@gmail.com>
2026-03-11 18:20:50 +0300

    Time: 140 ms (10.33%) | Memory: 36 MB (7.79%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -3,8 +3,8 @@ class Solution:
         stack = deque()
         res = [0]*len(temperatures)
         for idx,temp in enumerate(temperatures):
-            while stack and temp > temperatures[stack[-1]]:
-                popped = stack.pop()
+            while stack and temp > stack[-1][1]:
+                popped, _ = stack.pop()
                 res[popped] = idx-popped
-            stack.append(idx)
+            stack.append([idx,temp])
         return res
