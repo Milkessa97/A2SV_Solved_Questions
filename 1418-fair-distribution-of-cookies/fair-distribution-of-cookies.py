4cafb3c7ddd250e7bbbb9cceb65e4331e7c8b7f6
Milkessa Habtamu <milkessahabtamukebu@gmail.com>
2026-03-26 18:07:47 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (27.48%) - LeetSync

--- 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.py
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.py
@@ -3,20 +3,24 @@ class Solution:
         child = [0]*k
         fairness = float('inf')
 
-        def backtrack(idx,_max=0):
+        def backtrack(idx,curr_max=0):
             nonlocal fairness
             nonlocal child
-            if _max >= fairness:
+
+            if curr_max >= fairness:
                 return
 
             if idx == len(cookies):
-                fairness = min(fairness,_max)
+                fairness = min(fairness,curr_max)
                 return
 
             for i in range(k):
                 child[i] += cookies[idx]
-                backtrack(idx+1,max(_max,child[i]))
+                backtrack(idx+1,max(curr_max,child[i]))
                 child[i] -= cookies[idx]
+
+                if child[i] == 0:
+                    return
 
         backtrack(0)
         return fairness
